76e3b0df26acf558a5c7154cf2f337768c81e1e5
gy <marcusgao@163.com>
2016-05-01 17:09:21 +0800

    lab6

--- labcodes/lab6/kern/process/proc.c
+++ labcodes/lab6/kern/process/proc.c
@@ -136,9 +136,9 @@ alloc_proc(void) {
     proc->rq = NULL;
     list_init(&(proc->run_link));
     proc->time_slice = 0;
-    proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
+    skew_heap_init(&(proc->lab6_run_pool));
     proc->lab6_stride = 0;
-    proc->lab6_priority = 0;
+    proc->lab6_priority = 1;
     }
     return proc;
 }
--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -54,15 +54,15 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
-	extern uintptr_t __vectors[];
-	int i;
-	for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
-		SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
-	}
-	// set for switch from user to kernel
-	SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
-	// load the IDT
-	lidt(&idt_pd);
+     /* LAB5 YOUR CODE */
+     //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
+     //so you should setup the syscall interrupt gate in here
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++)
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -238,8 +238,9 @@ trap_dispatch(struct trapframe *tf) {
         ticks ++;
         if (ticks % TICK_NUM == 0) {
             print_ticks();
-            current->need_resched = 1;
+            sched_class_proc_tick(current);
         }
+
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
